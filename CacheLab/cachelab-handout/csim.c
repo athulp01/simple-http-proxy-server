9b2a497074dc510248e5637d6ded22e083005825
Athul P <athul929@gmail.com>
2020-04-27 13:17:12 +0530

    cache simulator finished

--- CacheLab/cachelab-handout/csim.c
+++ CacheLab/cachelab-handout/csim.c
@@ -1,3 +1,10 @@
+/* 
+ * E-way set-associative cache simulator
+ * LRU is the eviction policy
+ * Cache is represented as a 2D array of cache lines
+ * Uses valgrind style memory trace as input
+*/
+
 #define  _GNU_SOURCE
 #include "cachelab.h"
 #include <unistd.h>
@@ -11,36 +18,38 @@
 #define CACHE_MISS 1<<1
 #define CACHE_EVICTION 1<<2
 
-#define ADDR_LEN 31
+#define ADDR_LEN sizeof(long long)*8    // set maximum input address bit length to 8 byte
 
-#define SET_INDEX(addr, set_size, block_size) \
-        addr & ((-1>>(ADDR_LEN-set_size))<<block_size)
-#define BLOCK_OFFSET(addr, block_size) \
-        (addr & (-1>>(ADDR_LEN - block_size)))
-#define TAG(addr, set_size, block_size) \
-        addr & ((-1>>(ADDR_LEN-(set_size+block_size))<<(set_size + block_size)
+#define printf if(verbose) printf       //lazy to replace
 
-int num_of_lines;
+int num_of_lines;                       // number of cache lines in a set
 int num_of_sets, num_of_blocks;
-int misses=0, hits=0, evictions=0;
+int verbose=0;                          // verbose flag
+
+int misses=0, hits=0, evictions=0;      //Keep track of the counters
+
 char *trace_file;
 
+long counter = 0;                       //used for implementing lru
+
 struct cache_line{
-    int tag, count;
+    int tag;
+    long counter;
     char valid;
 }*cache;
 
 void evict(struct cache_line *cache, int set, int tag) {
-    int lru_idx = 0, min = INT_MAX;
+    int lru_idx = 0;
+    long min = LONG_MAX;
     for(int i=0; i<num_of_lines; i++) {
-        if(cache[i].count <= min) {
-            min = cache[i].count;
+        if(cache[i].counter <= min) {
+            min = cache[i].counter;
             lru_idx = i;
         }
     }
     cache[lru_idx].tag = tag;
     cache[lru_idx].valid = 1;
-    cache[lru_idx].count = 1;
+    cache[lru_idx].counter = ++counter;
 }
 
 void cache_search(struct cache_line *cache, int set, int tag) {
@@ -49,8 +58,8 @@ void cache_search(struct cache_line *cache, int set, int tag) {
     for(int i=0; i<num_of_lines; i++) {
         if(cache[i].valid == 0) empty_idx = i;
         if(cache[i].tag == tag && cache[i].valid == 1) {
-            cache[i].count++;
-            printf(" hit\n");
+            cache[i].counter = ++counter;
+            printf(" hit");
             hits++;
             return;
         }
@@ -63,16 +72,15 @@ void cache_search(struct cache_line *cache, int set, int tag) {
     }else {
         cache[empty_idx].tag = tag;
         cache[empty_idx].valid = 1;
-        cache[empty_idx].count = 1;
+        cache[empty_idx].counter = ++counter;
     }
-    printf("\n");
     misses++;
     return;
 }
 
 void set_opts(int argc, char *argv[]) {
     int opt;
-    while((opt = getopt(argc, argv, "s:E:b:t:"))!=-1) {
+    while((opt = getopt(argc, argv, "s:E:b:t:v"))!=-1) {
         switch (opt)
         {
         case 's':
@@ -86,6 +94,9 @@ void set_opts(int argc, char *argv[]) {
             break;
         case 't':
             trace_file = optarg;
+            break;
+        case 'v':
+            verbose = 1;
         default:
             break;
         }
@@ -95,29 +106,28 @@ void set_opts(int argc, char *argv[]) {
 void parse_trace(char *filename) {
     FILE *file = fopen(filename, "r");
     char mode, *line=NULL;
-    int addr, set ,tag, block;
+    unsigned long long addr;
+    int set ,tag, block;
     size_t len=0;
     while(-1 != getline(&line, &len, file)) {
         if(line[0] == 'I') continue;
         line[strcspn(line, "\n")] = 0;
         printf("%s ",line);
-        sscanf(line, " %c %x, %d", &mode, &addr, &block);
-        unsigned int tmp = ~(INT_MIN>>(ADDR_LEN-num_of_sets));
-        unsigned int mask = tmp<<num_of_blocks;
-        set = addr & mask;
-        set >>= num_of_blocks;
-        tmp = ~(INT_MIN>>(ADDR_LEN-(num_of_sets+num_of_blocks)));
-        mask = (tmp<<(num_of_sets + num_of_blocks));
-        tag = addr & mask;
-        tag >>= (num_of_blocks + num_of_sets);
+        sscanf(line, " %c %llx, %d", &mode, &addr, &block);
+        unsigned long long mask = (ULLONG_MAX>>(ADDR_LEN-num_of_sets));
+        set = (addr>>num_of_blocks) & mask;
+        mask = (ULLONG_MAX>>(ADDR_LEN-(num_of_sets+num_of_blocks)));
+        tag = (addr>>(num_of_sets + num_of_blocks)) & mask;
         switch(mode) {
             case 'L':
             case 'S':
                 cache_search(cache, set, tag);
+                printf("\n");
                 break;
             case 'M':
                 cache_search(cache, set, tag);
                 cache_search(cache, set, tag);
+                printf("\n");
                 break;
         }
     }
@@ -126,7 +136,7 @@ void parse_trace(char *filename) {
 int main(int argc, char *argv[])
 {
     set_opts(argc, argv);
-    size_t size = num_of_lines * 1<<num_of_sets * sizeof(struct cache_line);
+    size_t size = num_of_lines * (1<<num_of_sets) * sizeof(struct cache_line);
     cache = malloc(size);
     memset(cache, 0, size);
     parse_trace(trace_file);
